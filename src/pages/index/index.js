b0a56be09de2313dcaa8ba8d3a91ba6b8b1b450e
郭建利 <guojianli@ihappy.net.cn>
2020-10-18 11:26:40 +0800

    feat: 添加less

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,21 +1,11 @@
 import * as React from 'react';
 import { View, Text, Image } from 'remax/one';
-import styles from './index.css';
+import styles from './index.less';
 
 export default () => {
   return (
     <View className={styles.app}>
-      <View className={styles.header}>
-        <Image
-          src="https://gw.alipayobjects.com/mdn/rms_b5fcc5/afts/img/A*OGyZSI087zkAAAAAAAAAAABkARQnAQ"
-          className={styles.logo}
-          alt="logo"
-        />
-        <View className={styles.text}>
-          编辑 <Text className={styles.path}>src/pages/index/index.js</Text>{' '}
-          开始
-        </View>
-      </View>
+      <View className={styles.header}>header</View>
     </View>
   );
 };
