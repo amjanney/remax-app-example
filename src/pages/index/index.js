684ccea625131c870b978e788ab1174172af0ea3
郭建利 <guojianli@ihappy.net.cn>
2020-10-18 22:11:53 +0800

    feat：使用 useAsync

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,18 +2,25 @@ import * as React from 'react';
 import { View } from 'remax/one';
 import styles from './index.less';
 import { testGet, testPost } from '@/services/index';
+import useAsync from '@/hooks/useAsync';
 
 export default () => {
   React.useEffect(() => {
-    const param = 123;
-    testGet(param).then((res) => {
-      console.log('get==>', res);
-    });
-
-    testPost(param).then((res) => {
-      console.log('get==>', res);
-    });
+    /**
+     * 传统的写法
+     */
+    // const param = 123;
+    // testGet(param).then((res) => {
+    //   console.log('get==>', res);
+    // });
   }, []);
+
+  /**
+   * 使用useAsync的写法
+   * 只关注返回的值，逻辑处理由useAsync处理，使得代码看起来更加的简单
+   */
+  const [data, status, error] = useAsync(testGet, param);
+
   return (
     <View className={styles.app}>
       <View className={styles.header}>header</View>
